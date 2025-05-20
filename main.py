6e5db3749d94bb14cc6c22c656c93d05c0a2cd1b
vishudhGupta <vishudhgupta001@gmail.com>
2025-05-20 18:24:43 +0530

    port

--- main.py
+++ main.py
@@ -100,5 +100,5 @@ async def handle_appointment_response(request: Request):
         raise HTTPException(status_code=500, detail="Internal server error")
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.environ.get("PORT", "10000"))
     uvicorn.run(app, host="0.0.0.0", port=port)
